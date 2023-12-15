6b854c21413658a8ae963216bde80cd65fedadf6
Krish <krishrathor18@gmail.com>
2023-12-15 14:18:37 +0530

    created next js app and added ui a internal package

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,20 +1,7 @@
-"use client";
-
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+export const Button = () => {
+    return (
+        <div>
+            button
+        </div>
+    )
 }
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
