2fd13278c001115b8099678354b8efd5e64cc2c2
Krish <krishrathor18@gmail.com>
2024-01-05 21:59:23 +0530

    added ui for song card

--- apps/web/src/components/CardsProperties/SongCard/Properties.tsx
+++ apps/web/src/components/CardsProperties/SongCard/Properties.tsx
@@ -4,28 +4,49 @@ import { currentSelectedElement } from "@/atoms/elements/currentSelectedElement"
 import { Typography, TextField } from "@mui/material";
 import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
+import { OutlinedInput, OutlinedTextarea } from "../Card/Properties";
 
 export const ChangeSongCardProperties: React.FC = () => {
   const properties = [
-    { title: "width", value: 450, type: "number" },
-    { title: "height", value: 200, type: "number" },
     { title: "color", value: "#13274F", type: "string" },
-    { title: "cornerRadius", value: 2, type: "number" },
     { title: "headingColor", value: "#F0F8FF", type: "string" },
     { title: "subTextColor", value: "#B2BEB5", type: "string" },
     { title: "contentColor", value: "white", type: "string" },
-    { title: "headingFont", value: 25, type: "number" },
-    { title: "subTextFont", value: 15, type: "number" },
-    { title: "contentFont", value: 20, type: "number" },
-    { title: "headingText", value: "Sample Card", type: "string" },
-    { title: "subText", value: "Subtext goes here", type: "string" },
+    { title: "iconColor", value: "#F0F8FF", type: "string" },
+  ];
+
+  const PositionSizeProperties = [
+    { title: "width", value: 450, type: "number", name: "Width" },
+    { title: "height", value: 200, type: "number", name: "Height" },
+    { title: "cornerRadius", value: 2, type: "number", name: "Corner Radius" },
+  ];
+
+  const FontProperties = [
+    { title: "headingFont", value: 25, type: "number", name: "Heading Font" },
+    { title: "subTextFont", value: 15, type: "number", name: "SubText Font" },
+    { title: "contentFont", value: 20, type: "number", name: "Content Font" },
+  ];
+
+  const TextProperties = [
+    {
+      title: "headingText",
+      value: "Sample Card",
+      type: "string",
+      name: "Heading Text",
+    },
+    {
+      title: "subText",
+      value: "Subtext goes here",
+      type: "string",
+      name: "SubText",
+    },
     {
       title: "content",
       value:
         "This assumes that you are using these values as props in a React component.",
       type: "string",
+      name: "Content",
     },
-    { title: "iconColor", value: "#F0F8FF", type: "string" },
   ];
 
   const { number } = useRecoilValue(currentSelectedElement);
@@ -67,33 +88,88 @@ export const ChangeSongCardProperties: React.FC = () => {
 
   return (
     <div>
-      {properties.map((property) => (
-        <div key={property.title} className="flex text-white mt-4">
-          <label>{property.title}</label>
-          <TextField
-            id={property.title}
-            label="Standard"
-            variant="standard"
-            className="bg-white text-black ml-2"
-            onChange={(e) => {
-              const value = e.target.value;
-              handlePropertyChange(
-                property.title,
-                property.type === "number"
-                  ? value === ""
-                    ? 0
-                    : parseFloat(value)
-                  : value
-              );
-            }}
-            value={
-              cardProperties
-                .filter((card) => card.index === number)
-                .map((card) => (card as any)[property.title])[0]
-            }
-          />
-        </div>
-      ))}
+      <hr className=" mt-4 w-52 m-auto" />
+      <div>
+        <p className="mt-4 ml-2 text-xl">Postion & Size</p>
+        {PositionSizeProperties.map((property) => (
+          <div className="flex align-middle items-center" key={property.name} >
+            <p className="ml-6 m-6 inline-block w-24">{property.name}</p>
+            <OutlinedInput
+              onChange={(e) => {
+                const value = e.target.value;
+                handlePropertyChange(
+                  property.title,
+                  property.type === "number"
+                    ? value === ""
+                      ? 0
+                      : parseFloat(value)
+                    : value
+                );
+              }}
+              value={
+                cardProperties.filter((card) => card.index === number).map(
+                  (card) => (card as any)[property.title]
+                )[0] ?? property.value
+              }
+            />
+          </div>
+        ))}
+      </div>
+      <hr className=" mt-4 w-52 m-auto" />
+      <div>
+        <p className="mt-4 ml-2 text-xl">Text</p>
+        {TextProperties.map((property) => (
+          <div key={property.name} >
+            <p className="ml-24 mt-2 inline-block w-24">{property.name}</p>
+            <OutlinedTextarea
+              onChange={(e) => {
+                const value = e.target.value;
+                handlePropertyChange(
+                  property.title,
+                  property.type === "number"
+                    ? value === ""
+                      ? 0
+                      : parseFloat(value)
+                    : value
+                );
+              }}
+              value={
+                cardProperties.filter((card) => card.index === number).map(
+                  (card) => (card as any)[property.title]
+                )[0] ?? property.value
+              }
+            />
+          </div>
+        ))}
+      </div>
+      <hr className=" mt-4 w-52 m-auto" />
+      <div>
+        <p className="mt-4 ml-2 text-xl">Font</p>
+        {FontProperties.map((property) => (
+          <div className="flex align-middle items-center" key={property.name} >
+            <p className="ml-6 m-6 inline-block w-24">{property.name}</p>
+            <OutlinedInput
+              onChange={(e) => {
+                const value = e.target.value;
+                handlePropertyChange(
+                  property.title,
+                  property.type === "number"
+                    ? value === ""
+                      ? 0
+                      : parseFloat(value)
+                    : value
+                );
+              }}
+              value={
+                cardProperties.filter((card) => card.index === number).map(
+                  (card) => (card as any)[property.title]
+                )[0] ?? property.value
+              }
+            />
+          </div>
+        ))}
+      </div>
+      <hr className=" mt-4 w-52 m-auto" />
     </div>
   );
 };
